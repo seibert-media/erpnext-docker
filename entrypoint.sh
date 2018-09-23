9d18138d6d4b82c73157d0ddd6a5de03cb5cab35
Benjamin Borbe <bborbe@rocketnews.de>
2018-09-23 18:39:16 +0200

    * move bench commands after config modify * improve output

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,9 @@ if [ "$1" = 'bench' ] && [ "$2" = 'start' ]; then
 
 	echo "set ADMIN_PASSWORD to ***"
 	sed -i "s/{{ADMIN_PASSWORD}}/${ADMIN_PASSWORD}/" sites/common_site_config.json
-	bench set-admin-password "${ADMIN_PASSWORD}"
 
 	echo "set DB_HOST to ${DB_HOST}"
 	sed -i "s/{{DB_HOST}}/${DB_HOST}/" sites/common_site_config.json
-	bench set-mariadb-host "${DB_HOST}"
 
 	echo "set DB_NAME to ${DB_NAME}"
 	sed -i "s/{{DB_NAME}}/${DB_NAME}/" sites/site1.local/site_config.json
@@ -22,11 +20,15 @@ if [ "$1" = 'bench' ] && [ "$2" = 'start' ]; then
 	echo "set DB_PASSWORD to ***"
 	sed -i "s/{{DB_PASSWORD}}/${DB_PASSWORD}/" sites/site1.local/site_config.json
 
+	echo "set bench values"
+	bench set-mariadb-host "${DB_HOST}"
+	bench set-admin-password "${ADMIN_PASSWORD}"
+
 	echo "install apps ..."
 	bench --site site1.local install-app erpnext || true
 	bench --site site1.local install-app banana  || true
 
-	echo "run bench start"
+	echo "run $@"
 fi
 
 exec "$@"
