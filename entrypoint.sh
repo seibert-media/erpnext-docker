9e48b95e49566277d7580bb518385ff0d28c28b7
Benjamin Borbe <bborbe@rocketnews.de>
2018-09-23 18:23:49 +0200

    set admin password and db host via bench

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,11 @@ if [ "$1" = 'bench' ] && [ "$2" = 'start' ]; then
 
 	echo "set ADMIN_PASSWORD to ***"
 	sed -i "s/{{ADMIN_PASSWORD}}/${ADMIN_PASSWORD}/" sites/common_site_config.json
+	bench set-admin-password "${ADMIN_PASSWORD}"
 
 	echo "set DB_HOST to ${DB_HOST}"
 	sed -i "s/{{DB_HOST}}/${DB_HOST}/" sites/common_site_config.json
+	bench set-mariadb-host "${DB_HOST}"
 
 	echo "set DB_NAME to ${DB_NAME}"
 	sed -i "s/{{DB_NAME}}/${DB_NAME}/" sites/site1.local/site_config.json
