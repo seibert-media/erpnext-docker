53e850479d3ba699b0c8c8fbd691f93abf2df85b
Manuel Lorenz <mlorenz@seibert-media.net>
2018-11-15 11:06:19 +0100

    added script for pixelletter button

--- seibertmedia/fixtures/custom_scripts/Sales Invoice.js
+++ seibertmedia/fixtures/custom_scripts/Sales Invoice.js
@@ -1,1 +1,9 @@
-cur_frm.page.views.print.find(".btn-print-print").remove()
+cur_frm.page.views.print.find(".btn-print-print").remove();
+
+frappe.ui.form.on("Sales Invoice", "refresh", function(frm) {
+    if (frm.get_field("status").value == "Unpaid") {
+        frm.page.add_menu_item("Pixelletter", function() {
+            //TODO: Create draft letter entry
+        });
+    }
+});
