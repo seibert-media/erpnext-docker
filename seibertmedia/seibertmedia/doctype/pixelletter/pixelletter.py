77070293b7573384f3cb44c1fef86015ecbe2d9b
Manuel Lorenz <mlorenz@seibert-media.net>
2018-11-28 16:04:54 +0100

    added pdf writer

--- seibertmedia/seibertmedia/doctype/pixelletter/pixelletter.py
+++ seibertmedia/seibertmedia/doctype/pixelletter/pixelletter.py
@@ -9,12 +9,23 @@ from frappe import attach_print
 import json
 from six import string_types
 from frappe.utils import (get_url, get_formatted_email, cint,
-                          validate_email_add, split_emails, time_diff_in_seconds, parse_addr)
+                          validate_email_add, split_emails, time_diff_in_seconds, parse_addr, call_hook_method)
 from frappe.utils.file_manager import get_file
+from frappe.utils import get_files_path
+import os
+from six import text_type
 
 
 class Pixelletter(Document):
     pass
+
+def get_folder(self, folder_name, parent_folder="Home"):
+    return frappe.get_doc({
+        "doctype": "File",
+        "file_name": folder_name,
+        "is_folder": 1,
+        "folder": parent_folder
+    }).insert()
 
 @frappe.whitelist()
 def make(doctype=None, name=None, send_email=False, print_html=None, print_format=None, attachments='[]', print_letterhead=True, attach_field=None):
@@ -28,6 +39,11 @@ def make(doctype=None, name=None, send_email=False, print_html=None, print_forma
         "has_attachment": 1 if attachments else 0
     })
     comm.insert(ignore_permissions=True)
+
+    doc = frappe.get_doc(doctype, name)
+
+    if not doc.has_permission():
+        frappe.throw("Not permitted", frappe.PermissionError)
 
     if not doctype:
         # if no reference given, then send it against the communication
@@ -52,18 +68,31 @@ def make(doctype=None, name=None, send_email=False, print_html=None, print_forma
                     "attached_to_doctype": comm.doctype,
                     "attached_to_name": comm.name,
                     "attached_to_field": attach_field,
-                    "folder": "Home/Attachments",
                     "is_private": True,
                     "content": print_format_file.get("fcontent"),
-                    "decode": False})
+                    "decode": True})
+                write_file(_file)
+                _file.file_url = "/private/files/{0}".format(_file.file_name)
                 _file.save()
-
-
     frappe.db.commit()
 
     return {
         "name": comm.name,
     }
+
+def write_file(_file):
+    """write file to disk with a random name (to compare)"""
+    file_path = get_files_path(is_private=_file.is_private)
+
+    # create directory (if not exists)
+    frappe.create_folder(file_path)
+    # write the file
+    if isinstance(_file.content, text_type):
+        _file.content = _file.content.encode()
+    with open(os.path.join(file_path.encode('utf-8'), _file.file_name.encode('utf-8')), 'wb+') as f:
+        f.write(_file.content)
+
+    return get_files_path(_file.file_name, is_private=_file.is_private)
 
 def add_attachments(name, attachments):
     '''Add attachments to the given Communiction'''
@@ -78,3 +107,4 @@ def add_attachments(name, attachments):
             # save attachments to new doc
             save_url(attach.file_url, attach.file_name, "Pixelletter", name,
                      "Home/Attachments", attach.is_private)
+
