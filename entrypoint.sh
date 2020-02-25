a7e00a8d2676544160738193eb678c64b8dbf375
Benjamin Borbe <bborbe@rocketnews.de>
2020-02-25 20:24:51 +0100

    run entrypoints on more commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$1" = '/usr/bin/supervisord' ] || [ "$1" = 'bench' ]; then
+if [ "$1" = '/usr/bin/supervisord' ] || [ "$1" = 'bench' ] || [ "$1" = '/home/frappe/bench-repo/env/bin/uwsgi' ] || [ "$1" = '/usr/local/bin/bench' ] || [ "$1" = '/usr/bin/node' ] || [ "$1" = '/usr/sbin/nginx' ]; then
   for file in /entrypoints/*
   do
     bash "$file"
